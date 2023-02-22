37321cbac8deb58435d7252a02b3988d37f22ee3
Sergey-Bocharnikov <prokaznikov.s@yandex.ru>
2023-02-22 16:20:22 +0500

    add функцию get_1word_user update функцию get_word_user

--- str_func.py
+++ str_func.py
@@ -1,7 +1,18 @@
 def get_word_user():
+    """Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     words_user = input("Введите предложение\n")
     words_upper = words_user.upper()
     return words_upper
 
 
 print(get_word_user())
+
+
+def get_1word_user():
+    """Функция, которая делает заглавными первые буквы каждого слова в строке"""
+    words_user = input("Введите предложение\n")
+    words_title = words_user.title()
+    return words_title
+
+
+print(get_1word_user())
