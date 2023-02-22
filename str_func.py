111e0bfa0e83194a6bea64d51e921ebd189f5cf5
Sergey-Bocharnikov <125299677+Sergey-Bocharnikov@users.noreply.github.com>
2023-02-22 16:16:31 +0500

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,4 +1,5 @@
 def get_word_user():
+    "Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами"
     words_user = input("Введите предложение\n")
     words_upper = words_user.upper()
     return words_upper
