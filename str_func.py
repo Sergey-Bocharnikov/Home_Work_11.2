ea972a7d74a35db336365f22c241b6592965ac2d
Sergey-Bocharnikov <prokaznikov.s@yandex.ru>
2023-02-22 17:32:36 +0500

    debug функцию get_word_user debug функцию get_1word_user

--- str_func.py
+++ str_func.py
@@ -1,8 +1,11 @@
-def get_word_user():
+def get_word_user() -> str:
     """Функция, которая принимает на вход строку и возвращает ее со всеми заглавными буквами"""
-    words_user = input("Введите предложение\n")
-    words_upper = words_user.upper()
-    return words_upper
+    words_user = input("Введите предложение, сделаем все буквы заглавными!\n")
+    if words_user.isdigit():
+        return "Пишите буквами, а не цифрами"
+    else:
+        words_upper = words_user.upper()
+        return words_upper
 
 
 print(get_word_user())
@@ -10,9 +13,12 @@ print(get_word_user())
 
 def get_1word_user():
     """Функция, которая делает заглавными первые буквы каждого слова в строке"""
-    words_user = input("Введите предложение\n")
-    words_title = words_user.title()
-    return words_title
+    words_user = input("\nВведите предложение, сделаем первую букву каждого слова заглавной!\n")
+    if words_user.isdigit():
+        return "Пишите буквами, а не цифрами"
+    else:
+        words_title = words_user.title()
+        return words_title
 
 
 print(get_1word_user())
